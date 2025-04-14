0edbb8447cb3130d277f44e001df90a8129cb454
eugenpivo <epivovarciuc@gmail.com>
2025-04-14 11:10:01 +0300

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,1 +1,8 @@
 import products from '../data/products';
+<select>
+  {products.filter(p => p.categorie === "Geam").map(p => (
+    <option key={p.cod}>
+      {p.cod} – {p.denumire} ({p.dimensiune}) – {p.pret} MDL / {p.um}
+    </option>
+  ))}
+</select>
